2d7bbe083cfd64bd269a4341dc6ce5edbee7f46e
Ivan <apedoivan@gmail.com>
2025-11-07 23:23:33 +0100

    Little update

--- visualizer/app/layout.tsx
+++ visualizer/app/layout.tsx
@@ -8,24 +8,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'visualizer',
-  description: 'Lisp Interpreter Visualizer',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
+  description: 'Lisp Interpreter Visualizer'
 }
 
 export default function RootLayout({
